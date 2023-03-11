4fbe8e6b6a97bcea235cafd0da53d0f4c57e04b5
Nikky1092 <mariannaniko1092@gmail.com>
2023-03-11 13:48:34 +0000

    Excuse Generator Proyect

--- src/app.js
+++ src/app.js
@@ -7,5 +7,32 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
+  let who = ["The dog", "My grandma", "His turtle", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "the keys", "the car"];
+  let when = [
+    "before the class",
+    "right on time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying"
+  ];
+
+  function getArrayContent(x) {
+    let index = Math.floor(Math.random() * x.length);
+    let content = x[index];
+    return content;
+  }
+  function getRandomSentence() {
+    let whoValue = getArrayContent(who);
+    let actionValue = getArrayContent(action);
+    let whatValue = getArrayContent(what);
+    let whenValue = getArrayContent(when);
+    let element = document.getElementById("excuse");
+    element.innerHTML =
+      whoValue + " " + actionValue + " " + whatValue + " " + whenValue;
+  }
+  getRandomSentence();
+
   console.log("Hello Rigo from the console!");
 };
